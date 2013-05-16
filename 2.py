33c4c375562d46ae457dfcfb80b2de1c4ef9c105
Oskar Öberg <oskar@thefarm.se>
2013-05-16 21:46:36 +0200

    crazyfication

--- 2.py
+++ 2.py
@@ -1,10 +1,10 @@
 from __future__ import division
 
 cur, prev, sum = 2, 1, 0
-
 while cur < 4000000:
-	if cur % 2 == 0:
-		sum += cur
-	prev, cur = cur, cur + prev
+	prev, cur, sum = cur, cur + prev, sum + cur if cur % 2 == 0 else sum
 
 print sum
+
+
+# 4613732
