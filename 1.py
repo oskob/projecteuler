63bb1c7142cd2e90bee9689cb7e48b4a10c54584
Oskar Öberg <oskar@thefarm.se>
2013-05-16 17:31:46 +0200

    1

---
+++ 1.py
@@ -0,0 +1,22 @@
+from __future__ import division
+
+limit = 1000
+nums = [3, 5]
+sum = 0
+
+for num in nums:
+	cur = num
+	numSum = 0
+	while cur < limit:
+		used = False
+		for tNum in nums:
+			if tNum < num:
+				if cur/tNum == cur//tNum:
+					used = True
+					break;
+		if not used:
+			numSum += cur
+		cur += num
+	sum += numSum
+	
+print sum
--- README.md
+++ README.md
@@ -2,3 +2,8 @@ projecteuler-python
 ===================
 
 Brain, INITIATE
+
+# Warning
+
+May contain super intelligent information, too hot to handle.
+Proceed at own risk.
